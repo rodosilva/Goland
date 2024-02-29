fbef6a2bb6fff250c9252dd876041c2903204433
rodosilva <rodosilva.alpha@gmail.com>
2024-02-28 22:16:32 -0500

    Basic Styling 2

--- hello-world/handlers.go
+++ hello-world/handlers.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"net/http"
 )
 
@@ -14,13 +12,4 @@ func Home(w http.ResponseWriter, r *http.Request) {
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("Error parsing template:", err)
-		return
-	}
 }
