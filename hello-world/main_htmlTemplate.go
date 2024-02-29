3b1ad7b9baa7ed946b4d9610b1bf871cbe4a3b9a
rodosilva <rodosilva.alpha@gmail.com>
2024-02-28 21:53:30 -0500

    Basic Styling

--- hello-world/main_htmlTemplate.go
+++ hello-world/main_htmlTemplate.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 )
 
 // const can't be changed
@@ -12,25 +11,6 @@ const portNumber = ":8080"
 // In order for a function to respond for a web browser
 // It needs to have 2 parameters
 // Response parameter and a request
-
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("Error parsing template:", err)
-		return
-	}
-}
 
 // main is the main application function
 func main() {
