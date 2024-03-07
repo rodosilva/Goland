c77055b875c52fbdabd015e284bd097d0295919e
rodosilva <rodosilva.alpha@gmail.com>
2024-03-06 21:28:41 -0500

    simple template cache

--- hello-world/cmd/web/main.go
+++ hello-world/cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"myapp/pkg/handlers"
 	"net/http"
 )
 
@@ -15,8 +16,8 @@ const portNumber = ":8080"
 // main is the main application function
 func main() {
 
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil) // If there is an error, I don't care. That means _
